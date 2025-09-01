37007b527028a8a3c041a8a3a5d9252a289b9462
thayM <thay.contato1@gmail.com>
2025-08-31 22:39:55 -0300

    🐛 fix: finalização do componente dos botões

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,18 +1,59 @@
 import React from 'react';
+import styles from './Button.module.scss';
 
 interface ButtonProps {
-    icon?: string;
-    positionText?: string;
-    color: string;
+    icon?: React.ComponentType<any>;
+    positionItens?: string;
+    colorIcon?: string;
+    colorText?: string;
+    backgrandColor?: string;
+    colorSombra?: string;
+    height: string;
+    width: string;
+    isAdm: boolean;
     tittle: string;
+    nameClass?: string; //para a manipulação dos svg caso necessário
 }
 
-const Button: React.FC<ButtonProps> = ({ icon, color, tittle }) => {
+const Button: React.FC<ButtonProps> = ({nameClass, colorText, colorSombra, icon: IconComponent, isAdm, positionItens, colorIcon, backgrandColor, height, width, tittle }) => {
+    const buttonStyle = {
+        '--color-icon': colorIcon,
+        '--color-text': colorText,
+        '--bg-color': backgrandColor,
+        '--height': height,
+        '--width': width,
+        '--position-text': positionItens,
+        '--color-sombra': colorSombra
+    } as React.CSSProperties;
+    
     return (
-        <button>
-            <div className="conteudo">{icon} {tittle}</div>
+        <>
+        <button style={buttonStyle} className={isAdm ? `${styles.button} ${styles.button__adm}` : `${styles.button} ${styles.button__user_comum}`}>
+        {IconComponent ? (
+            <div className={styles.button__icon}> 
+                <IconComponent class={nameClass}/>
+            </div>
+        ) : null}
+            <div className={styles.button__title}>{tittle}</div>
         </button>
-    );
+        </>
+    )
 };
 
 export default Button;
+
+// exemplo de chamada:
+//     <Button
+//     // colorIcon="#229CFF"
+//     colorText="#FFFFFF"
+//     backgrandColor='#229CFF'
+//     colorSombra='#0073D2'
+//     height="40px"
+//     width="400px"
+//     isAdm={true}
+//     tittle="Minhas rotas"
+//     // positionItens="center"
+//     // icon={IconRoute}
+// />
+
+// OBS: no positionItens coloque start caso houver um icon, senão não há necessidade de chamar esse atributo

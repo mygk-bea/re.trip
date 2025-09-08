05c7d2835bdc54fc4287c9f73240255623a72296
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2025-09-08 00:01:47 -0300

    ♻️ refactor: rota da página inicio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
         <Routes>
-          <Route path="/" element={<Inicio />} />
+          <Route path="/inicio" element={<Inicio />} />
           <Route path="/user/meu-perfil" element={<MeuPerfil type='Usuário Comum' username='Username' call='Aventureira' />} />
           <Route path="/user/meu-perfil/minhas-rotas" element={<MinhasRotas />} />
           <Route path="/user/meu-perfil/rotas-favoritas" element={<RotasFavoritas />} />

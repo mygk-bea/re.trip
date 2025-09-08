c2baa0191ec24b558a486173955a783b22c05803
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2025-09-08 00:32:12 -0300

    ✨ add: base da página inicio

--- frontend/src/pages/Inicio/index.tsx
+++ frontend/src/pages/Inicio/index.tsx
@@ -3,40 +3,40 @@ import styled from './Inicio.module.scss';
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
 
-interface InicioProps {
-
-}
+interface InicioProps {}
 
 const Inicio: React.FC<InicioProps> = () => {
     const navigate = useNavigate();
 
     return (
         <div className={`${styled.container}`}>
-            <h1 className={styled.title}>Re.Trip</h1>
+            <div className={`${styled.backgroundTitle}`}>
+                <div className={`${styled.overlay}`}>
+                    <h1 className={styled.title}>Re.Trip</h1>
+                </div>
+            </div>
             <div className={`${styled.content}`}>
                 <Button
-                    colorIcon="#ee8047"
-                    colorText="#363837"
-                    backgrandColor="#FFFFFF"
-                    colorSombra="#ee8047"
-                    height="50px"
+                    colorText="#fff9f6"
+                    backgrandColor="#ff7022"
+                    colorSombra="#dd3603"
+                    height="60px"
                     width="100%"
                     isAdm={false}
                     tittle="Cadastre-se"
-                    positionItens="start"
-                    onClick={() => navigate("/user/meu-perfil/minhas-rotas")}
+                    positionItens="center"
+                    onClick={() => navigate("/cadastro")}
                 />
                 <Button
-                    colorIcon="#ee8047"
-                    colorText="#363837"
+                    colorText="#ff7022"
                     backgrandColor="#FFFFFF"
                     colorSombra="#ee8047"
-                    height="50px"
+                    height="60px"
                     width="100%"
                     isAdm={false}
                     tittle="Entrar"
-                    positionItens="start"
-                    onClick={() => navigate("/user/meu-perfil/minhas-rotas")}
+                    positionItens="center"
+                    onClick={() => navigate("/login")}
                 />
             </div>
         </div>

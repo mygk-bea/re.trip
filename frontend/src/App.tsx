23342b99eb1f1c1c11330f61f8cd88358c4abe43
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2025-09-08 16:06:25 -0300

    ✨ add: estrutura pag cadastro e pasta images to public

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import MeuPerfil from './pages/MeuPerfil'
 import MinhasRotas from './pages/MeuPerfil/MinhasRotas'
 import RotasFavoritas from './pages/MeuPerfil/RotasFavoritas'
 import Inicio from './pages/Inicio';
+import Cadastro from './pages/Inicio/Cadastro';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
     <>
         <Routes>
           <Route path="/inicio" element={<Inicio />} />
+          <Route path="/criar-conta" element={<Cadastro />} />
           <Route path="/user/meu-perfil" element={<MeuPerfil type='Usuário Comum' username='Username' call='Aventureira' />} />
           <Route path="/user/meu-perfil/minhas-rotas" element={<MinhasRotas />} />
           <Route path="/user/meu-perfil/rotas-favoritas" element={<RotasFavoritas />} />

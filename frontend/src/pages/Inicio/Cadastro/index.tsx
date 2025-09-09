ad4e98ef257c791ccf80dc465aa738cb3162d0bf
Beatriz Meyagusko <100007869+mygk-bea@users.noreply.github.com>
2025-09-08 23:17:19 -0300

    ✨ add: pag cadastro responsiva

--- frontend/src/pages/Inicio/Cadastro/index.tsx
+++ frontend/src/pages/Inicio/Cadastro/index.tsx
@@ -1,13 +1,63 @@
 import React from "react";
 import styled from '../Inicio.module.scss';
 import styled_page from './Cadastro.module.scss';
-import Button from "../../../components/Button";
-import { useNavigate } from "react-router-dom";
+import type { Field } from "../../../types/field";
+import ModalForm from "../modalForm";
 
 interface CadastroProps {}
 
+const fields: Field[] = [
+    {
+        label: "Nome Completo",
+        type: "text",
+        placeholder: "Seu nome...",
+        name: "nome",
+    },
+    {
+        label: "Data de Nascimento",
+        type: "date",
+        placeholder: "dd/mm/aaaa",
+        name: "dataNascimento",
+    },
+    {
+        label: "Gênero",
+        type: "select",
+        placeholder: "Selecione...",
+        name: "genero",
+        options: [
+            { value: "masculino", label: "Masculino" },
+            { value: "feminino", label: "Feminino" },
+            { value: "outro", label: "Outro" },
+        ],
+    },
+    {
+        label: "E-mail",
+        type: "email",
+        placeholder: "exemplo@dominio.com",
+        name: "email",
+    },
+    {
+        label: "Senha",
+        type: "password",
+        placeholder: "Insira uma senha forte...",
+        name: "senha",
+    },
+];
+
 const Cadastro: React.FC<CadastroProps> = () => {
-    const navigate = useNavigate();
+    const modalForm = (
+        <div className={`${styled_page.modal} 
+            p-[20px_25px_30px]
+            w-[90vw] lg:w-[40vw] max-h-[85vh]
+            flex flex-col justify-center align-center gap-[2vh]
+            mx-auto
+            bg-[#FFF]
+            rounded-[50px]`}
+        >
+            <h1 className={`${styled_page.title} font-[Madimi_One] text-[#ff7022ff]`}>Cadastre-se</h1>
+            <ModalForm fields={fields} />
+        </div>
+    )
 
     return (
         <div className={`${styled.container} 
@@ -17,56 +67,21 @@ const Cadastro: React.FC<CadastroProps> = () => {
         >
             <div className="lg:w-1/2 flex justify-center">
                 <div className={`${styled.backgroundTitle} 
-                    h-screen w-[900px] lg:w-[50vw]
+                    h-screen w-screen lg:w-[50vw]
                     overflow-hidden 
                     lg:rounded-b-[0] lg:rounded-br-[500px]
                     bg-center bg-no-repeat bg-[auto_115%]
                     lg:shadow-[-10px_20px_0_20px_rgba(255,112,34,1)]`}
                 >
-                    <div className={`${styled.overlay} h-full w-full flex flex-col justify-center align-center`}>
-                        <h1 className={`${styled.title} font-[Madimi_One] lg:hidden text-[#FFF] z-[2]`}>Re.Trip</h1>
-
-                        <div className={`${styled_page.modal} 
-                            h-[60vh] w-[90vw] 
-                            flex justify-center align-center 
-                            bg-[#FFF] z-[2]
-                            rounded-[50px]`}
-                        >
-                            <h1 className={`${styled_page.title} font-[Madimi_One] text-[#ff7022ff]`}>Cadastro</h1>
-                        </div>
+                    <div className={`${styled.overlay} h-full w-full flex flex-col justify-center align-center lg:justify-start`}>
+                        <h1 className={`${styled.title} ${styled_page.logo} font-[Madimi_One] text-[#FFF] z-[2] lg:pt-[10vh]`}>Re.Trip</h1>
+                        <div className="block lg:hidden z-[2]">{modalForm}</div>
                     </div>
 
                 </div>
             </div>
             <div className={`${styled.content} w-[90.1vw] lg:w-[35vw] flex flex-col items-center gap-[4.2vh]`}>
-                <h1 className={`${styled.title} font-[Madimi_One] text-[#ff7022ff] hidden lg:block`}>Re.Trip</h1>
-
-                <Button
-                    colorText="#fff9f6"
-                    backgroundColor="#ff7022ff"
-                    colorShadow="#dd3603"
-                    height="60px"
-                    width="100%"
-                    isAdm={false}
-                    title="Cadastre-se"
-                    positionItems="center"
-                    fontSize="1.75rem"
-                    fontFamily="'Madimi One', sans-serif"
-                    onClick={() => navigate("/cadastro")}
-                />
-                <Button
-                    colorText="#ff7022"
-                    backgroundColor="#FFFFFF"
-                    colorShadow="#ee8047"
-                    height="60px"
-                    width="100%"
-                    isAdm={false}
-                    title="Entrar"
-                    positionItems="center"
-                    fontSize="1.75rem"
-                    fontFamily="'Madimi One', sans-serif"
-                    onClick={() => navigate("/login")}
-                />
+                <div className="hidden lg:block z-[2]">{modalForm}</div>
             </div>
         </div>
     );
---
+++ frontend/src/pages/Inicio/modalForm.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import Input from "../../components/Input";
+import type { Field } from "../../types/field";
+import Button from "../../components/Button";
+import { useNavigate } from "react-router-dom";
+
+interface ModalFormProps {
+    fields?: Field[];
+}
+
+export default function ModalForm({fields = []}: ModalFormProps) {
+    const [formData, setFormData] = useState<Record<string, string>>({});
+    const navigate = useNavigate();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Dados enviados:", formData);
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className="w-[100%] mx-auto flex flex-col items-center justify-center"
+        >
+
+        {fields.map((field) => (
+            <Input
+                key={field.name}
+                label={field.label}
+                type={field.type}
+                placeholder={field.placeholder}
+            />
+        ))}
+
+        <Button
+            colorText="#ff7022ff"
+            backgroundColor="#fff9f6"
+            colorShadow="#ff7022ff"
+            height="50px"
+            width="200px"
+            isAdm={false}
+            title="Salvar"
+            positionItems="center"
+            fontSize="1.25rem"
+            fontFamily="'Madimi One', sans-serif"
+            onClick={() => navigate("/")}
+        />
+        </form>
+    );
+}
+
+/*
+<Input
+    label="Confirme sua senha"
+    type="password"
+    placeholder="Confirme sua senha..."
+    name="confirmarSenha"
+    value={formData.confirmarSenha}
+    onChange={handleChange}
+/>
+
+button type submit
+*/

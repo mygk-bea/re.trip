284692f3c0f168a2a6fca493d914d80cb22d5a16
liabueno <juliavbueno13@gmail.com>
2025-09-09 10:12:41 -0300

    :bug: fix: props do componente input

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -4,27 +4,32 @@ interface InputProps {
     label: string;
     type: string;
     placeholder: string;
+    name: string;
+    value: any;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<InputProps> = ({ label, type, placeholder }) => {
+const Input: React.FC<InputProps> = ({ label, type, placeholder, name, value, onChange }) => {
     return (
         <div className='w-full'>
             <label className="block font-semibold mb-1 text-black text-left">{label}</label>
             <input
-                id={label}
-                name={label}
+                id={name}
+                name={name}
                 type={type}
                 placeholder={placeholder}
+                value={value}
+                onChange={onChange}
                 className="w-[100%] border-b border-gray-400 focus:outline-none focus:border-orange-500 pb-4 pl-3 mb-6 mt-4 text-black placeholder-gray-400"
             />
         </div>
     );
 };
 
-    //   <Input
-    //     label="Digite seu nome"
-    //     type="text"
-    //     placeholder="Seu nome..."
-    //   />
+//   <Input
+//     label="Digite seu nome"
+//     type="text"
+//     placeholder="Seu nome..."
+//   />
 
 export default Input;

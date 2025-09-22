ae64562d4462facbe4a3a075a6198f094251c7d9
liabueno <juliavbueno13@gmail.com>
2025-09-22 14:02:47 -0300

    :bug: fix: correção nome da pasta 1/2

---
+++ frontend/src/pagesCerto/User/Filtros/index.tsx
@@ -0,0 +1,146 @@
+import { useState } from "react";
+import styles from './Filtros.module.scss'
+import Tag from '../../../components/Tag';
+import Menu from '../../../components/Menu';
+import IconArrowChevron from '../../../assets/icons/icon-arrow-chevron';
+import { useNavigate } from "react-router-dom";
+import Button from "../../../components/Button";
+
+const Filtros: React.FC = () => {
+    const navigate = useNavigate();
+
+    const [selectedTags, setSelectedTags] = useState<{ text: string, bgColor: string, borderColor: string, textColor: string }[]>([]);
+
+    const handleTagClick = (tag: { text: string, bgColor: string, borderColor: string, textColor: string }) => {
+        setSelectedTags((prevTags) =>
+            prevTags.some(t => t.text === tag.text)
+                ? prevTags.filter(t => t.text !== tag.text)
+                : [...prevTags, tag]
+        );
+    };
+
+    const tagsAtracoes = [
+        { text: "Local Turístico", bgColor: "#F9F0E5", borderColor: "#BF6600", textColor: "#BF6600" },
+        { text: "Rota Turística", bgColor: "#F9F0E5", borderColor: "#BF6600", textColor: "#BF6600" },
+        { text: "Gastronônimica", bgColor: "#F9F0E5", borderColor: "#BF6600", textColor: "#BF6600" },
+        { text: "Cultural", bgColor: "#F9F0E5", borderColor: "#BF6600", textColor: "#BF6600" },
+        { text: "Compras", bgColor: "#F9F0E5", borderColor: "#BF6600", textColor: "#BF6600" },
+        { text: "Natureza e Ecoturismo", bgColor: "#EBF8E6", borderColor: "#34B505", textColor: "#34B505" },
+        { text: "Aventura e Diversão", bgColor: "#F4E5F9", borderColor: "#8F00BF", textColor: "#8F00BF" },
+        { text: "Histórico", bgColor: "#F4E5F9", borderColor: "#8F00BF", textColor: "#8F00BF" },
+        { text: "Religioso", bgColor: "#F4E5F9", borderColor: "#8F00BF", textColor: "#8F00BF" },
+    ];
+
+    const tagsLocalizacao = [
+        { text: "Bofete", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Boituva", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Botucatu", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Cesário Lange", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Cerquilho", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Itapetininga", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Tatuí", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Tietê", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Torre de Pedra", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Outras", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Centro", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Próximo à natureza", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Urbano", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+        { text: "Transporte Público", bgColor: "#F1F1F1", borderColor: "#757575", textColor: "#757575" },
+    ];
+
+    const tagsAceInc = [
+        { text: "Vaga Comum", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Vaga DeFis", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Cadeirante", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Braile", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Áudio Descrição", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Pet Friendly", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Banheiros", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+        { text: "Elevadores e Rampas", bgColor: "#E5E8FB", borderColor: "#001DD7", textColor: "#001DD7" },
+    ];
+
+    return (
+        <>
+            <div className={styles.container__filtros}>
+                <div className={styles.filtros__cabecalho}>
+                    <div className={styles.cabecalho__icon_titulo} >
+                        <button className={styles.icon__btn} onClick={() => navigate(-1)} >
+                            <IconArrowChevron class={styles.icon__arrow} />
+                        </button>
+                        <h1>Filtros</h1>
+                    </div>
+                    <div className={styles.cabecalho__divisor}></div>
+                </div>
+
+                <div className={styles.filtros__conteudos}>
+                    <div className={styles.filtros__atracoes}>
+                        <div className={styles.atracoes__titulo}><h2>Atrações</h2></div>
+                        <div className={styles.atracoes__conteudo}>
+                            {tagsAtracoes.map((tag, index) => (
+                                <Tag
+                                    key={index}
+                                    text={tag.text}
+                                    bgColor={tag.bgColor}
+                                    borderColor={tag.borderColor}
+                                    textColor={tag.textColor}
+                                    onClick={() => handleTagClick(tag)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className={styles.filtros__localizacao}>
+                        <div className={styles.localizacao__titulo}><h2>Localização</h2></div>
+                        <div className={styles.localizacao__conteudo}>
+                            {tagsLocalizacao.map((tag, index) => (
+                                <Tag
+                                    key={index}
+                                    text={tag.text}
+                                    bgColor={tag.bgColor}
+                                    borderColor={tag.borderColor}
+                                    textColor={tag.textColor}
+                                    onClick={() => handleTagClick(tag)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+
+                    <div className={styles.filtros__acessibilidade_inclusao}>
+                        <div className={styles.acessibilidade__titulo}><h2>Acessibilidade e Inclusão</h2></div>
+                        <div className={styles.acessibilidade__conteudo}>
+                            {tagsAceInc.map((tag, index) => (
+                                <Tag
+                                    key={index}
+                                    text={tag.text}
+                                    bgColor={tag.bgColor}
+                                    borderColor={tag.borderColor}
+                                    textColor={tag.textColor}
+                                    onClick={() => handleTagClick(tag)}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                {/* Botão de confirmação */}
+                <div className="flex justify-end mt-10">
+                    <Button
+                        colorText="#FFFFFF"
+                        height="40px"
+                        width="250px"
+                        isAdm={false}
+                        title="Confirmar Filtros"
+                        positionItems="center"
+                        onClick={() => navigate('/user/pesquisar', { state: { selectedTags } })}
+                    />
+                </div>
+
+                <div>
+                    <Menu />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Filtros;
---
+++ frontend/src/pagesCerto/User/Inicio/Login/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from '../Inicio.module.scss';
+import styled_page from '../Forms/Form.module.scss';
+import type { Field } from "../../../../types/field";
+import ModalForm from "../Forms/modalForm";
+
+interface LoginProps {}
+
+const fields: Field[] = [
+    {
+        label: "E-mail",
+        type: "email",
+        placeholder: "exemplo@dominio.com",
+        name: "email",
+    },
+    {
+        label: "Senha",
+        type: "password",
+        placeholder: "Insira uma senha forte...",
+        name: "senha",
+    },
+];
+
+const Login: React.FC<LoginProps> = () => {
+    const modalForm = (
+        <div className={`${styled_page.modal} 
+            p-[3.2vh_25px]
+            w-[90vw] lg:w-[40vw] 
+            flex flex-col justify-center align-center gap-[2vh]
+            mx-auto
+            `}
+        >
+            <h1 className={`${styled_page.title} font-[Madimi_One] text-[#ff7022ff]`}>Bem-vindo!</h1>
+            <ModalForm fields={fields} />
+        </div>
+    )
+
+    return (
+        <div className={`${styled.container} 
+            h-screen w-screen 
+            overflow-hidden 
+            flex flex-col lg:flex-row items-center`}
+        >
+            <div className="lg:w-1/2 flex justify-center">
+                <div className={`${styled.backgroundTitle} 
+                    h-screen w-screen lg:w-[50vw]
+                    overflow-hidden 
+                    lg:rounded-b-[0] lg:rounded-br-[500px]
+                    bg-center bg-no-repeat bg-[auto_115%]
+                    lg:shadow-[-10px_20px_0_20px_rgba(255,112,34,1)]`}
+                >
+                    <div className={`${styled.overlay} h-full w-full flex flex-col justify-center align-center lg:justify-start relative`}>
+                        <h1 className={`${styled.title} ${styled_page.logo} 
+                            font-[Madimi_One] text-[#FFF] 
+                            z-[2] absolute left-1/2 -translate-x-1/2 top-[5vh] 
+                            lg:pt-[10vh]`}>Re.Trip</h1>
+                        <div className="block lg:hidden z-[2] max-h-[80vh] mt-[5vh] lg:mt-0 overflow-y-auto bg-[#FFF] rounded-[50px] mx-auto">{modalForm}</div>
+                    </div>
+
+                </div>
+            </div>
+            <div className={`${styled.content} w-[90.1vw] lg:w-[35vw] flex flex-col items-center gap-[4.2vh]`}>
+                <div className="hidden lg:block z-[2]">{modalForm}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
